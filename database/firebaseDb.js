24ae3ee9367866718fecaf86e76ade20a7e28dc5
Unword112 <pog123@hotmail.co.uk>
2022-01-18 14:36:06 +0700

    'react-native-crud-with-patiphan'

---
+++ database/firebaseDb.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCOt1lSwtHb6LW4LAu7KCtMoAn7a2PqnrE",
+    authDomain: "proj-app-8f628.firebaseapp.com",
+    databaseURL: "https://proj-app-8f628.firebaseio.com",
+    projectId: "proj-app-8f628",
+    storageBucket: "proj-app-8f628.appspot.com",
+    messagingSenderId: "516925451113",
+    appId: "1:516925451113:web:0867043b126eacb6b42bf8",
+    measurementId: "G-KVHKVQY1BS"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export const firestore = firebase.firestore();
+export default firebase;
